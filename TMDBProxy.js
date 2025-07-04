dbcfa6215de120a68a8cb9f72a5ee09d767b7161
Bylumpa <john050882@ya.ru>
2025-07-04 22:59:04 +0300

    Update TMDBProxy.js

--- TMDBProxy.js
+++ TMDBProxy.js
@@ -33,7 +33,7 @@
 
     Lampa.TMDB.api = function (url) {
       var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
-      return Lampa.Storage.field('proxy_tmdb') ? 'https://lam.maxvol.pro/api/3/' + account(url) : base;
+      return Lampa.Storage.field('proxy_tmdb') ? 'https://lam.maxvol.pro/tmdb/api/3/' + account(url) : base;
     };
 
     Lampa.Settings.listener.follow('open', function (e) {
